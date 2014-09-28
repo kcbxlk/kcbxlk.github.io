da35bb4400323726992148a42af0ea8f7ebf036a
caok <caok1231@gmail.com>
2014-09-28 10:06:01 +0800

    add mobile detect

--- js/main.js
+++ js/main.js
@@ -4,4 +4,13 @@ $(function(){
     durion: 1000,
     hash: true
   });
+
+  var isIPhone = /iPhone/i.test(navigator.userAgent),
+    isIPad = /iPad/i.test(navigator.userAgent),
+    isAndroid = /android/i.test(navigator.userAgent);
+  var isMobile = isIPhone || isIPad || isAndroid;
+
+  if (isMobile){
+    $("ul#navigate").remove();
+  }
 });
